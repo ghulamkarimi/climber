5f5cafaee94b819d4fe019e6c6f80f0da06ab391
ghulamkarimi <karimiamirhossein12@gmail.com>
2024-08-01 08:53:30 +0200

    update

--- controllers/CategorieController.ts
+++ controllers/CategorieController.ts
@@ -16,7 +16,7 @@ export const getAllCategories = asyncHandler(async (req: Request, res: Response)
         res.json(error)
     }
 })
- 
+
 export const createCategory = asyncHandler(async (req, res) => {
     const { email, categories, title, price, size, bewertung, bild } = req.body;
 
@@ -48,7 +48,39 @@ export const createCategory = asyncHandler(async (req, res) => {
     }
 });
 
+export const editCategories = asyncHandler(async (req, res) => {
 
-  
+    const { email, categories, title, price, size, bewertung, bild } = req.body;
+
+    console.log('Request Body:', req.body); // Log the request body
+
+    try {
+        const userExist = await Users.findOne({ email });
+        if (!userExist) {
+            throw new Error('User not found');
+        }
+
+        if (userExist.isAdmin === true) {
+            const category = await Categories.findByIdAndUpdate(req.params.id, {
+                categories,
+                title,
+                price,
+                size,
+                bewertung,
+                bild
+            });
+
+            res.status(201).json({ category, message: "Category updated successfully" });
+        } else {
+            res.status(403).json({ message: "You are not admin" });
+        }
+    } catch (error) {
+        console.error('Error updating category:', error); // Log the error
+        res.status(500).json({ message: error.message });
+    }
+});
+
+
+
 
 
--- routes/categoriesRouter.ts
+++ routes/categoriesRouter.ts
@@ -1,11 +1,12 @@
 import Router from 'express';
 
-import { createCategory, getAllCategories } from '../controllers/CategorieController';
+import { createCategory, getAllCategories, editCategories } from '../controllers/CategorieController';
 
 const categoriesRouter = Router();
 
 categoriesRouter.get("/getall" , getAllCategories);
 categoriesRouter.post("/create", createCategory);
+categoriesRouter.put("/edit/:id", editCategories);
 
 
 export default categoriesRouter;
